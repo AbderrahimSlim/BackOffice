ce9f7dbe59bed6899132bb144b749e2d75926e47
ahmedhabchi <ahmed.habchi@esprit.tn>
2014-10-09 00:14:42 +0200

    fdsd

--- appClientModule/com/massconnections/Delegate/UserCrudDelegate.java
+++ appClientModule/com/massconnections/Delegate/UserCrudDelegate.java
@@ -1,26 +1,34 @@
 package com.massconnections.Delegate;
 
-import com.massconnections.Services.UserCrudEJBRemote;
+import com.massconnections.Domains.Crowd;
+import com.massconnections.Services.CrowdCrudEJBRemote;
 import com.massconnections.locator.ServiceLocator;
 
 public class UserCrudDelegate {
-	/*private static UserCrudEJBRemote cart;
+	private static CrowdCrudEJBRemote cart;
 
-	private static UserCrudEJBRemote getRemoteEJB() {
-		cart = (UserCrudEJBRemote) ServiceLocator.getInstance().getProxy("/OnlineStore/PersonneCrudEJB!edu.esprit.onlineStore.services.PersonneCrudEJBRemote");
+	private static CrowdCrudEJBRemote getRemoteEJB() {
+		cart = (CrowdCrudEJBRemote) ServiceLocator
+				.getInstance()
+				.getProxy(
+						"/massconnection-ejb/UserCrudEJB!com.massconnections.Services.UserCrudEJBRemote");
 		return cart;
 	}
 
-	public static void addItem(Item item) {
-		getRemoteEJB().add(item);
+	public static void addCrowd(Crowd crowd) {
+		getRemoteEJB().addCrowd(crowd);
 	}
 
-	public static void resetItems() {
-		getRemoteEJB().resetCart();
+	public static Crowd getCrowd(int id) {
+		return getRemoteEJB().getById(id);
 	}
 
-	public static float gettotalPriece() {
-		System.out.println(getRemoteEJB().getTotalprice());
-		return getRemoteEJB().getTotalprice();
-	}*/
+	public static void main(String[] args) {
+
+		Crowd c = new Crowd();
+		c.setLastName("aaa");
+		
+
+	}
+
 }
