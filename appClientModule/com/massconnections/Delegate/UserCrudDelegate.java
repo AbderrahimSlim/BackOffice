3321db97a7f9d2368d2f101dd6d871ae194deb4a
Youssef <Youssef@172.16.206.154>
2014-10-08 23:37:12 +0100

    service locator + config

---
+++ appClientModule/com/massconnections/Delegate/UserCrudDelegate.java
@@ -0,0 +1,26 @@
+package com.massconnections.Delegate;
+
+import com.massconnections.Services.UserCrudEJBRemote;
+import com.massconnections.locator.ServiceLocator;
+
+public class UserCrudDelegate {
+	/*private static UserCrudEJBRemote cart;
+
+	private static UserCrudEJBRemote getRemoteEJB() {
+		cart = (UserCrudEJBRemote) ServiceLocator.getInstance().getProxy("/OnlineStore/PersonneCrudEJB!edu.esprit.onlineStore.services.PersonneCrudEJBRemote");
+		return cart;
+	}
+
+	public static void addItem(Item item) {
+		getRemoteEJB().add(item);
+	}
+
+	public static void resetItems() {
+		getRemoteEJB().resetCart();
+	}
+
+	public static float gettotalPriece() {
+		System.out.println(getRemoteEJB().getTotalprice());
+		return getRemoteEJB().getTotalprice();
+	}*/
+}
