51d86b6601feea22535e8c56c0b730298e58853a
ahmedhabchi <ahmed.habchi@esprit.tn>
2014-10-09 21:09:37 +0200

    fds

---
+++ appClientModule/com/massconnections/locator/ServiceLocatorException.java
@@ -0,0 +1,14 @@
+package com.massconnections.locator;
+
+public class ServiceLocatorException extends RuntimeException {
+	
+	private static final long serialVersionUID = 6744464404205649419L;
+	
+
+	
+
+	public ServiceLocatorException(Throwable cause) {
+		super(cause);
+	}
+
+}
