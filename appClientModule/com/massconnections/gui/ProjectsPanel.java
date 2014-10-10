dee794bebfa517dda941ab41e48a9d8baf95dbf2
Youssef <Youssef@Farouk-PC.esprit.tn>
2014-10-10 01:35:52 +0100

    aze

---
+++ appClientModule/com/massconnections/gui/ProjectsPanel.java
@@ -0,0 +1,14 @@
+package com.massconnections.gui;
+
+import javax.swing.JPanel;
+
+public class ProjectsPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public ProjectsPanel() {
+
+	}
+
+}
